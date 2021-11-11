d41bb25c4ae0c7c0b6d0a3ef1fee0e14952855d7
David Leblanc <lblancdavid@gmail.com>
2021-11-11 06:04:21 -1100

    Restructuring the project a little bit

---
+++ PokerSim.Bots/SimpleRandomPlayer.cs
@@ -0,0 +1,45 @@
+﻿using PokerSim.Engine.Game;
+using PokerSim.Engine.Players;
+using System;
+
+namespace PokerSim.Bots
+{
+    public class SimpleRandomPlayer : IPlayer
+    {
+        private double _checkCallProb;
+        private double _raiseProb;
+        private Random _random = new Random();
+
+        public SimpleRandomPlayer(string name, double checkCallProb, double raiseProb)
+        {
+            _checkCallProb = checkCallProb;
+            _raiseProb = raiseProb;
+            Name = name;
+            Id = Guid.NewGuid();
+        }
+
+        public string Name { get; private set; }
+
+        public Guid Id { get; private set; }
+
+        public TurnResult TakeTurn(IPlayerTurnState state)
+        {
+            double p = _random.NextDouble();
+            if (p < _raiseProb)
+            {
+                return TurnResult.Raise(this, state.Blinds * 3);
+            }
+            if (p < _raiseProb + _checkCallProb)
+            {
+                return TurnResult.CheckOrCall(this);
+            }
+
+            if (state.CurrentBet == 0)
+            {
+                return TurnResult.CheckOrCall(this);
+            }
+            return TurnResult.Fold(this);
+
+        }
+    }
+}
--- PokerSim.TestConsole/Program.cs
+++ PokerSim.TestConsole/Program.cs
@@ -1,5 +1,5 @@
-﻿using PokerSim.Engine.Game;
-using PokerSim.Engine.Players;
+﻿using PokerSim.Bots;
+using PokerSim.Engine.Game;
 using System;
 
 namespace PokerSim.TestConsole
@@ -9,8 +9,8 @@ namespace PokerSim.TestConsole
         static void Main(string[] args)
         {
             var engine = new TexasHoldemGameEngine(new ConsoleGameEventLogger(true));
-            engine.AddPlayer(new BasicRandomPlayer("Bob", 0.25, 0.5, 0.25));
-            engine.AddPlayer(new BasicRandomPlayer("Joe", 0.25, 0.5, 0.25));
+            engine.AddPlayer(new SimpleRandomPlayer("Bob", 0.25, 0.25));
+            engine.AddPlayer(new SimpleRandomPlayer("Joe", 0.25, 0.25));
             //engine.AddPlayer(new BasicRandomPlayer("Jane", 0.25, 0.5, 0.25));
             //engine.AddPlayer(new BasicRandomPlayer("Frank", 0.25, 0.5, 0.25));
 
